da178484253c3052566382b3b770db7a96add615
Rui Yang <ryang@bglcorp.com.au>
2021-08-21 22:27:02 +1000

    first commit

---
+++ src/main/java/me/ryang/jooqexample/JooqExampleApplication.java
@@ -0,0 +1,13 @@
+package me.ryang.jooqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ryang/jooqexample/JooqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ryang.jooqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
